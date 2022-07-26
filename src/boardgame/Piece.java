a14aa992b699e904bbb4ee2fdd45560e52e3481e
LeonardoIzau <leo.izau@gmail.com>
2022-07-26 02:57:16 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,30 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	/*
+	public boolean[][] possibleMoves(){
+		
+	}
+	
+	public boolean possibleMove(Position position) {
+		
+	}
+	
+	public boolean isThereAnyPossibleMove(){
+		
+	}
+	*/
+}
